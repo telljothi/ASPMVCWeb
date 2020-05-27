bc1a88d3a98b9674b385010e82758ae63f2e4c9e
jothi <telljothi@gmail.com>
2020-05-27 20:50:36 +0300

    Commit

--- ASPMVCWeb/App_Start/BundleConfig.cs
+++ ASPMVCWeb/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace ASPMVCWeb
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            //
         }
     }
 }
